32eea8f9db154f77ad77b8440cf52d922f64cc68
Hasham Younas <hashamyounis9@gmail.com>
2024-08-18 19:54:52 +0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, jsonify
+import nodriver as uc
+from bs4 import BeautifulSoup
+import asyncio
+
+
+app = Flask(__name__)
+
+
+async def scrape(q):
+    browser = await uc.start()
+    page = await browser.get(f"https://google.com/search?q={q}")
+    titles = []
+    try:
+        # global title
+        titles = await page.select_all('h3')
+    except:
+        return {"error": "Some error occured while performing query",
+                        "hint": "Maybe your query is invalid/unclear"}
+
+    elements = await page.select_all('.N54PNb.BToiNc.cvP2Ce')
+    searche_results = elements
+
+    results = []
+    i = 1
+    for result in searche_results:
+        search = {}
+        soap = BeautifulSoup(await result.get_html(), 'html.parser')
+        anchor = soap.find('a')
+        if anchor:
+            search['site_url'] = anchor.get('href')
+        else:
+            search['site_url'] = None
+        title = soap.find("h3")
+        if title:
+            search['result_heading'] = title.text.strip()
+        else:
+            search['result_heading'] = None
+        site_name = soap.find(class_="VuuXrf")
+        if site_name:
+            search['result_by_site'] = site_name.text.strip()
+        else:
+            search['result_by_site'] = None
+        overview = soap.find(
+            class_="VwiC3b yXK7lf lVm3ye r025kc hJNv6b Hdw6tb")
+        if overview:
+            search['result_overview'] = overview.text.strip()
+        else:
+            search['result_overview'] = None
+        img = soap.find('img')
+        if img:
+            search['site_logo'] = img.get('src')
+        else:
+            search['img'] = None
+        results.append(search)
+    browser.stop()
+    return results
+
+
+@app.route('/')
+def index():
+    q = request.args.get('q')
+    if not q:
+        return jsonify({"Error": "q (argument to send query to api) is missing...",
+                        "Suggestion": "Try again :)",
+                        "Usage": "https://api_url/?q={your_query}"})
+    else:
+        result = uc.loop().run_until_complete(scrape(q))
+        return jsonify(result)
+    return jsonify({"Error": "some unknown error occured..."})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
